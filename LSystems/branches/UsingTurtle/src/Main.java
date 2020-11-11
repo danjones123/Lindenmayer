da418f7a1b3bd5a9c14e1ea865160ef4abc6a2ce
zfac207 <zfac207@f67d8ccb-98e5-4c2f-8c90-870bcd0e286d>
2020-11-11 18:00:11 +0000

    Added ability to save the current position, which can then be used for travelling back and forth down an LSystem.

--- LSystems/branches/UsingTurtle/src/Main.java
+++ LSystems/branches/UsingTurtle/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
   public static void main(String[] args) {
     Display display = new Display();
     display.setup();
-    Turtle turtle = new Turtle("F+F+F+F-F+F+F+F",30, 45, 200, 200);
+    Turtle turtle = new Turtle("F[[F]+F]++F",30, 90, 200, 200);
     turtle.rules();
     display.screen();
   }
